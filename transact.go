5e9263f2e15e0ce1914b5fed58528d0d36a6b07e
Jason Frame <jason@onehackoranother.com>
2018-08-14 00:48:15 +0100

    tabs -> spaces

--- transact.go
+++ transact.go
@@ -1,40 +1,40 @@
 package transact
 
 import (
-	"context"
-	"database/sql"
+    "context"
+    "database/sql"
 )
 
 type Transaction interface {
-	Exec(query string, args ...interface{}) (sql.Result, error)
-	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
-	Prepare(query string) (*sql.Stmt, error)
-	PrepareContext(ctx context.Context, query string) (*sql.Stmt, error)
-	Query(query string, args ...interface{}) (*sql.Rows, error)
-	QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
-	QueryRow(query string, args ...interface{}) *sql.Row
-	QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row
+    Exec(query string, args ...interface{}) (sql.Result, error)
+    ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
+    Prepare(query string) (*sql.Stmt, error)
+    PrepareContext(ctx context.Context, query string) (*sql.Stmt, error)
+    Query(query string, args ...interface{}) (*sql.Rows, error)
+    QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
+    QueryRow(query string, args ...interface{}) *sql.Row
+    QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row
 }
 
 type TxFn func(Transaction) error
 
 func Transact(db *sql.DB, fn TxFn) (err error) {
-	tx, err := db.Begin()
-	if err != nil {
-		return
-	}
+    tx, err := db.Begin()
+    if err != nil {
+        return
+    }
 
-	defer func() {
-		if p := recover(); p != nil {
-			tx.Rollback()
-			panic(p)
-		} else if err != nil {
-			tx.Rollback()
-		} else {
-			err = tx.Commit()
-		}
-	}()
+    defer func() {
+        if p := recover(); p != nil {
+            tx.Rollback()
+            panic(p)
+        } else if err != nil {
+            tx.Rollback()
+        } else {
+            err = tx.Commit()
+        }
+    }()
 
-	err = fn(tx)
-	return err
+    err = fn(tx)
+    return err
 }
